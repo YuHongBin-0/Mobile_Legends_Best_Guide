7899c195c8a7225d4ae694a36129ab40bb608b6f
Muhd Fitri <fitzgeralxz@gmail.com>
2020-02-24 19:39:53 +0800

    ds

--- app/src/main/java/com/example/mobilelegendsbestguide/MainActivity.java
+++ app/src/main/java/com/example/mobilelegendsbestguide/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.mobilelegendsbestguide;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 

15048904f847b937222cbe46ae02d94241891aa2
YuHongBin-0 <1801187d@student.tp.edu.sg>
2020-02-24 17:37:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mobilelegendsbestguide/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobilelegendsbestguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
